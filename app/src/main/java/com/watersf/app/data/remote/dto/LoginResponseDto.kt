77dee50720b79c2d0b727791e2f201883f758a05
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2026-06-19 21:15:03 -0600

    a

--- app/src/main/java/com/watersf/app/data/remote/dto/LoginResponseDto.kt
+++ app/src/main/java/com/watersf/app/data/remote/dto/LoginResponseDto.kt
@@ -3,7 +3,7 @@ package com.watersf.app.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponseDto(
-    @SerializedName("accessToken") val accessToken: String?,
-    @SerializedName("token") val token: String?,
+    @SerializedName(value = "accessToken", alternate = ["access_token", "access_Token"]) val accessToken: String?,
+    @SerializedName(value = "token", alternate = ["Token"]) val token: String?,
     @SerializedName("user") val user: JwtUser
 )
